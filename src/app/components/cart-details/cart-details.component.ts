4b14d2cffc987ecd7b5b286a1df15870286aa549
Glendonh <glendon.hardin@gmail.com>
2023-04-14 11:35:02 -0700

    adjust cart qty

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -3,6 +3,16 @@ import { CommonModule } from '@angular/common';
 import { Product } from '../../services/product.service';
 import { Cart } from '../../services/cart.service';
 
+interface CartProduct extends Product {
+  qty: number;
+  subTotal: number;
+}
+
+interface DetailedCart {
+  cartProducts: CartProduct[];
+  totalPrice: number;
+}
+
 @Component({
   standalone: true,
   selector: 'app-cart-details',
@@ -14,10 +24,38 @@ export class CartDetailsComponent {
   constructor() {}
   @Input() products: Product[];
   @Input() cart: Cart;
-  detailedCart;
+  detailedCart: DetailedCart;
+  adjustQty(productId: number, newQuantity: number) {
+    if (newQuantity === 0) {
+      const newProducts = this.detailedCart.cartProducts.filter(
+        (product) => product.id !== productId
+      );
+      this.detailedCart = {
+        totalPrice: this.findTotalPrice(newProducts),
+        cartProducts: newProducts,
+      };
+    } else {
+      const newProducts: CartProduct[] = this.detailedCart.cartProducts.map(
+        (product) => {
+          if (product.id === productId) {
+            return {
+              ...product,
+              qty: newQuantity,
+              subTotal: newQuantity * product.price,
+            };
+          }
+          return product;
+        }
+      );
+      this.detailedCart = {
+        totalPrice: this.findTotalPrice(newProducts),
+        cartProducts: newProducts,
+      };
+    }
+  }
   ngOnChanges() {
     if (this.cart && this.products) {
-      const cartProducts = this.cart.products.map((prod) => {
+      const cartProducts: CartProduct[] = this.cart.products.map((prod) => {
         const product = this.products.find((p) => p.id === prod.productId);
         return {
           ...product,
@@ -25,11 +63,14 @@ export class CartDetailsComponent {
           subTotal: prod.quantity * product.price,
         };
       });
-      const totalPrice = cartProducts.reduce((acc, product) => {
-        acc += product.subTotal;
-        return acc;
-      }, 0);
+      const totalPrice = this.findTotalPrice(cartProducts);
       this.detailedCart = { cartProducts, totalPrice };
     }
+  }
+  findTotalPrice(cartProducts: CartProduct[]) {
+    return cartProducts.reduce((acc, product) => {
+      acc += product.subTotal;
+      return acc;
+    }, 0);
   }
 }
