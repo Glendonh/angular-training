52df88e9042b254d3f819163839e5b331ce31e21
Glendonh <glendon.hardin@gmail.com>
2024-01-08 12:12:33 -0800

    add cart count to navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { selectCartCount } from 'src/app/reducers';
 
 @Component({
   selector: 'app-navbar',
@@ -9,10 +11,12 @@ import { Observable } from 'rxjs';
 })
 export class NavbarComponent implements OnInit {
   @Input() name: string;
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private _store: Store) {}
   isAuthenticated: Observable<boolean>;
+  cartCount: Observable<number>;
 
   ngOnInit() {
     this.isAuthenticated = this.authService.getAuthObservable();
+    this.cartCount = this._store.select(selectCartCount)
   }
 }
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -11,18 +11,28 @@ import { cartReducer } from './cart.reducer'
 import { Product } from '../services/product.service';
 import { DetailedCart } from '../services/cart.service';
 
+export interface State {
+  products: Product[];
+  cart: DetailedCart;
+}
+
 export const selectProducts = createFeatureSelector<Product[]>('products');
 export const selectCart = createFeatureSelector<DetailedCart>('cart');
+export const selectCartCount = createSelector(selectCart, (cart: DetailedCart) => {
+  if (cart?.cartProducts) {
+    return cart?.cartProducts.reduce((acc, product) => {
+      return acc + product.qty;
+    }, 0)
+  }
+  return 0;
+})
+
 
 const debug = (reducer: ActionReducer<any>): ActionReducer<any> => {
   return (state, action) => {
     console.log({state, action})
     return reducer(state, action)
   }
-}
-
-export interface State {
-
 }
 
 export const reducers: ActionReducerMap<State> = {
